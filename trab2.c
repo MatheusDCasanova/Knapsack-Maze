3ffb1667b31d498821dbc8cddee0391c12be9f94
Matheus Casanova <matheuscasanova02@gmail.com>
2022-06-14 23:17:05 -0300

    codigo ja encontra a melhor solucao, falta implementar print do caminho

--- trab2.c
+++ trab2.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 
 
+int max(int v1, int v2){
+    if (v1 > v2){
+        return v1;
+    }
+    return v2;
+}
 
-void solve(int ** toxi, int ** peso, int n, int c){
+
+int solve(int ** toxi, int ** peso, int n, int c){
     // funcao que resolve o problema:
     /*
     - para cada elemento (i,j,c) temos a solucao otima para ir de (0,0) ate (i,j) em uma mochila de capacidade c
     - temos que encontrar o maximo entre os seguintes casos:
-        1) considerando que veio de cima e coletando o atual (isso se o peso do atual for menor que c)
+        1) considerando que veio de cima e coletando o atual (isso somente se o peso do atual for menor que c)
         2) considerando que veio de cima e nao coletando o atual
-        3) considerando que veio da esquerda e coletando o atual (isso se o peso do atual for menor que c)
+        3) considerando que veio da esquerda e coletando o atual (isso somente se o peso do atual for menor que c)
         4) considerando que veio da esquerda e nao coletando o atual
     - eh importante notar que caso esteja na borda esquerda ou superior, a solucao para o de cima ou da esquerda eh 0
     */    
@@ -41,20 +48,31 @@ void solve(int ** toxi, int ** peso, int n, int c){
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++){
             for (int k = 1; k <= c; k++){
-                if (peso[i][j] <= k){
-                    //considerando que coleto o atual:
-                    sol[i][j][k] = 
+                if (peso[i-1][j-1] <= k){ //se couber o atual
+                    //considerando que coleto o atual e veio de cima
+                    sol[i][j][k] =  toxi[i-1][j-1] + sol[i-1][j][k - peso[i-1][j-1]];
+
+                    //considerando nao coleto atual e veio de cima
+                    sol[i][j][k] = max(sol[i][j][k], sol[i-1][j][k]); //ja pega o maximo entre coletando o atual ou n
+
+
+                    //considerando coleto o atual e veio da esquerda
+                    sol[i][j][k] = max(sol[i][j][k], toxi[i-1][j-1] + sol[i][j-1][k-peso[i-1][j-1]]);
+
+
+                    //considerando nao coleto o atual e veio da esquerda
+                    sol[i][j][k] = max(sol[i][j][k], sol[i][j-1][k]);
+
+                } else{
+                    //venho de cima e n coleto o atual (ja que o peso do atual eh maior q a capacidade)
+                    sol[i][j][k] = sol[i-1][j][k];
+
+                    //comparo com vindo da esquerda
+                    sol[i][j][k] = max(sol[i][j][k], sol[i][j-1][k]);
                 }
             }
         }
     }
-
-
-
-
-
-
-
 
 /*for (int i = 0; i <= n; i++){
     for (int j = 0; j <= n; j++){
@@ -77,13 +95,9 @@ void solve(int ** toxi, int ** peso, int n, int c){
         }
     }
 */
+
+    return sol[n][n][c];
 }
-
-
-    
-}
-
-
 
 
 int main() {
@@ -107,13 +121,9 @@ int main() {
         for(int j = 0; j < n; ++j)
             scanf("%d", &peso[i][j]);
 
-    
 
-    // para exemplificar a saida
-    int value = 42;
-    char *solution = "CDDCBCBC";
-
-    printf("%d\n%s\n", value, solution);
+    int resultado = solve(toxi, peso, n, c);
+    printf("%d\n", resultado);
 
     return 0;
 }
